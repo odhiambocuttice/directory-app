4745a72466a88d8e3a8e58fe74ee4a930d928476
cuttice <odhiambocuttice@gmail.com>
2022-05-21 00:39:19 +0300

    Add Models

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,3 +1,4 @@
+import numbers
 from django.utils import timezone
 from django.db import models
 
@@ -5,10 +6,10 @@ from django.db import models
 class Registration(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     email = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=255, blank=False)
-    OTP = models.CharField(max_length=4, blank=False)
+    phone_number = models.IntegerField(blank=False)
+    OTP = models.IntegerField(blank=False)
     created = models.DateTimeField(db_index=True, default=timezone.now)
     updated = models.DateTimeField(db_index=True, default=timezone.now)
-
+ 
     def __str__(self):
         return self.email
