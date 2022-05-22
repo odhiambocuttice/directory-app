b20100d33324072dc130cf3f7748346dcb213963
cuttice <odhiambocuttice@gmail.com>
2022-05-22 21:22:36 +0300

    Restructure app

---
+++ directory/local_setting.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dashboard',
+        'USER': 'dashboard',
+        'PASSWORD': 'dashboard',
+        'HOST': 'localhost'
+    }
+}
