21398330d80fb1157e438ba34e9b0397f1ec6501
cuttice <odhiambocuttice@gmail.com>
2022-05-22 22:36:03 +0300

    fix urls

--- directory/urls.py
+++ directory/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('dashboard.urls')),
     # re_path('.*',TemplateView.as_view(template_name='index.html')),
-    re_path(r"^$", TemplateView.as_view(template_name='index.html'))
-    # path('api-auth/', include('rest_framework.urls')),
+    re_path("", TemplateView.as_view(template_name='index.html'))
 ]
